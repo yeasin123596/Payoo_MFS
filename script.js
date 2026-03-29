d933d7a8aba465bb01d52e169ea332c3f6d9b935
yeasin123596 <yeasin123596@gmail.com>
2026-03-29 17:53:28 +0600

    Event listener added to the login button

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById("login-button");
+
+loginButton.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log("LogIn Button Clicked");
+})

9c0beb8635146772582c921be9d2f436f9e78833
yeasin123596 <yeasin123596@gmail.com>
2026-03-29 18:17:44 +0600

    LogIn functionality added and home page setup  completed

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ const loginButton = document.getElementById("login-button");
 
 loginButton.addEventListener('click', function(event){
     event.preventDefault();
-    console.log("LogIn Button Clicked");
+    
+    const mobileNumber = 1234567890;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById("mobile-number").value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+    
+    const pinNumberValue = document.getElementById("pin-number").value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    if ( mobileNumber === mobileNumberValueConverted  && pinNumber === pinNumberValueConverted) {
+        window.location.href="./home.html";
+    } else {
+        alert("Invalid Credentials");
+    }
 })

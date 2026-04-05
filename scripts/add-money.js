bbd186cb5b1429f5ffca32abd20a813d147c2e8c
yeasin123596 <yeasin123596@gmail.com>
2026-04-05 13:41:10 +0600

    logout fetaure added

--- scripts/add-money.js
+++ scripts/add-money.js
@@ -1,12 +1,4 @@
-// Log Out Button
 
-document.getElementById("logout-button")
-.addEventListener('click', function(event){
-    event.preventDefault();
-
-    window.location.href="./index.html"
-
-})
 
 
 
---
+++ scripts/logout.js
@@ -0,0 +1,9 @@
+// Log Out Button
+
+document.getElementById("logout-button")
+.addEventListener('click', function(event){
+    event.preventDefault();
+
+    window.location.href="./index.html"
+
+})

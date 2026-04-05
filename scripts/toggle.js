f959e2d59e377b3a4bd9e3bf54ad1285361ab3d5
yeasin123596 <yeasin123596@gmail.com>
2026-04-05 18:19:23 +0600

    toggle feature  design added

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,44 +1,52 @@
+// Function to handle toggle
+function handelToggle (id) {
+    const forms = document.getElementsByClassName("form");
+    for(const form of forms) {
+        form.style.display = "none";
+    }
+
+    document.getElementById(id).style.display = "block";
+}
+
+// Function to change style when selected
+function changeStyle(id) {
+    const buttons = document.getElementsByClassName("service-btn")  
+        for (const btn of buttons) {
+            btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+            btn.classList.add("border-gray-300");
+        }
+    document.getElementById(id).classList.remove("border-gray-300");   
+    document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+}
+
+
+
 // Add Money Toggle
 document.getElementById("add-money-service").addEventListener('click', function(){
-    document.getElementById("add-money-form").style.display = 'block';
-    document.getElementById("cash-out-form").style.display = 'none';
-    document.getElementById("transfer-money-form").style.display = 'none';
-    document.getElementById("get-bonus-form").style.display = 'none';
-    document.getElementById("pay-bill-form").style.display = 'none';
+    handelToggle("add-money-form");
+    changeStyle("add-money-service");
 })
 
 // Cash Out Toggle
 document.getElementById("cash-out-service").addEventListener('click', function(){
-    document.getElementById("cash-out-form").style.display = 'block';
-    document.getElementById("add-money-form").style.display = 'none';
-    document.getElementById("transfer-money-form").style.display = 'none';
-    document.getElementById("get-bonus-form").style.display = 'none';
-    document.getElementById("pay-bill-form").style.display = 'none';
+    handelToggle("cash-out-form");
+    changeStyle("cash-out-service");
 })
 
 // Transfer Money Toggle
 document.getElementById("transfer-money-service").addEventListener('click', function(){
-    document.getElementById("transfer-money-form").style.display = 'block';
-    document.getElementById("add-money-form").style.display = 'none';
-    document.getElementById("cash-out-form").style.display = 'none';
-    document.getElementById("get-bonus-form").style.display = 'none';
-    document.getElementById("pay-bill-form").style.display = 'none';
+    handelToggle("transfer-money-form");
+    changeStyle("transfer-money-service");
 })
 
 // Get Bonus Toggle 
 document.getElementById("get-bonus-service").addEventListener('click', function(){
-    document.getElementById("get-bonus-form").style.display = 'block';
-    document.getElementById("add-money-form").style.display = 'none';
-    document.getElementById("cash-out-form").style.display = 'none';
-    document.getElementById("transfer-money-form").style.display = 'none';
-    document.getElementById("pay-bill-form").style.display = 'none';
+    handelToggle("get-bonus-form");
+    changeStyle("get-bonus-service");
 })
 
 // Pay bill Toggle
 document.getElementById("pay-bill-service").addEventListener('click', function(){
-    document.getElementById("pay-bill-form").style.display = 'block';
-    document.getElementById("add-money-form").style.display = 'none';
-    document.getElementById("cash-out-form").style.display = 'none';
-    document.getElementById("transfer-money-form").style.display = 'none';
-    document.getElementById("get-bonus-form").style.display = 'none';
+    handelToggle("pay-bill-form");
+    changeStyle("pay-bill-service");
 })
